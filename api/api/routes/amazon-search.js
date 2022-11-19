7f19b3a64873b9ed64ace4d7e161e2c99e003353
Matthew Liu <mattliu1728@gmail.com>
2022-11-19 14:11:13 -0500

    Feature: amazon search (#5)
    * Create index.js
    
    * Create amazon-search.js
    
    * Update index.js
    
    * Update index.js
    
    * remove console log

---
+++ api/api/routes/amazon-search.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const auth = require('../../utils/auth');
+const { getAmazonResults } = require("../../puppeteer");
+
+router.get('/', auth, async (req, res) => {
+    // api/?q=mysearchquery
+
+    try {
+        const data = await getAmazonResults(req.query.q);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Something went wrong." })
+        process.exit(1);
+    }
+});
+
+module.exports = router;
---
+++ api/puppeteer/index.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+
+/**
+ * returns top 3 urls from amazon.ca based on a text input
+ * @param {String} textInput 
+ */
+const getAmazonResults = async textInput => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // page.on('console', msg => console.log(msg.text())); // uncomment for NodeJS debugging
+
+    await page.goto("https://www.amazon.ca");
+
+    // Type into search box.
+    const searchBarSelector = '.nav-input';
+    await page.waitForSelector(searchBarSelector);
+    await page.type(searchBarSelector, textInput);
+
+    // click amazon search
+    const searchButtonSelector = '.nav-search-submit';
+    await page.waitForSelector(searchButtonSelector);
+    await Promise.all([
+        page.click(searchButtonSelector),
+        page.waitForNavigation({ waitUntil: "networkidle2" }),
+    ]);
+
+    // Wait for the results page to load and display the results.
+    const resultsSelector = '.s-product-image-container';
+    await page.waitForSelector(resultsSelector);
+
+    // Extract the results from the page.
+    const links = await page.evaluate(() => {
+        return [...document.querySelectorAll("h2")]
+            .filter(r => r.querySelector('a')) // has anchor element?
+            .map(r => {
+                return "https://www.amazon.ca" + r.querySelector('a').getAttribute("href")
+            }) // get href element
+    });
+    await browser.close();
+    return links.slice(0, 3);
+};
+
+module.exports = {
+    getAmazonResults
+}
