b7a0b002fc9e4faeb5b1e86b45824b9145d0f56f
aasirvalji <aasir.valji@hotmail.com>
2022-11-19 15:52:52 -0500

    added sockets

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 const morgan = require('morgan');
@@ -19,13 +20,36 @@ if (nodeEnv === 'development') {
   app.use(morgan('dev'));
 }
 
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  });
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
+
 // routes
 const apiPrefix = './api/routes';
 app.use('/api/auth', require(`${apiPrefix}/auth.js`));
 app.use('/api/pills', require(`${apiPrefix}/pills.js`));
 app.use('/api/aws', require(`${apiPrefix}/aws.js`));
 
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(`Server running in ${nodeEnv} on port ${PORT}`.yellow.bold)
 );
 
