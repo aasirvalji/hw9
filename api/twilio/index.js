03b12f3867fb55e747f92bf2afb41d4755a94e8b
Matt <mattliuusa@hotmail.com>
2022-11-19 15:21:30 -0500

    create notify relative

--- api/twilio/index.js
+++ api/twilio/index.js
@@ -1,8 +1,14 @@
+const path = require("path");
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-    .create({ body: 'Hi there', from: '+18655688025', to: '+15199651728' })
-    .then(message => console.log(message.sid));
+const notifyRelative = message => {
+    client.messages
+        .create({ body: message, from: '+18655688025', to: '+15199651728' })
+        .then(message => console.log(message.sid));
+};
 
+module.exports = { notifyRelative };
