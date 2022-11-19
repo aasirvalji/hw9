8cb1b66319ec1f2112eb27ff14e3ebdfe610580d
aasirvalji <aasir.valji@hotmail.com>
2022-11-19 14:13:36 -0500

    remove geo

--- api/db/models/Appointment.js
+++ api/db/models/Appointment.js
@@ -10,24 +10,10 @@ const Appointment = new mongoose.Schema({
     ref: 'patient',
   },
   date: {
-    type: Date,
+    type: String,
   },
-  location: {
-    // GeoJSON Point
-    type: {
-      type: String,
-      enum: ['Point'],
-    },
-    coordinates: {
-      type: [Number],
-      index: '2dsphere',
-    },
-    formattedAddress: String,
-    street: String,
-    city: String,
-    state: String,
-    zipcode: String,
-    country: String,
+  time: {
+    type: String,
   },
 });
 
