db1408efff2e42410dcc36f2bfe2b461b9215b4d
aasirvalji <aasir.valji@hotmail.com>
2022-11-20 03:25:57 -0500

    updated user server flow

--- api/twilio/index.js
+++ api/twilio/index.js
@@ -1,14 +1,17 @@
-const path = require("path");
+const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-const notifyRelative = message => {
-    client.messages
-        .create({ body: message, from: '+18655688025', to: '+15199651728' })
-        // .then(message => console.log(message.sid));
+const notifyRelative = (message) => {
+  client.messages.create({
+    body: message,
+    from: '+18655688025',
+    to: '+16479867448',
+  });
+  // .then(message => console.log(message.sid));
 };
 
 module.exports = { notifyRelative };
