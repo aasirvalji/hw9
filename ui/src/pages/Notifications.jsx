952053af76848f8746b4c56bf48843095e345255
mattt-liu <mattliuusa@hotmail.com>
2022-11-20 03:17:42 -0500

    clean up

--- ui/src/pages/Notifications.jsx
+++ ui/src/pages/Notifications.jsx
@@ -3,22 +3,6 @@ import React, { useEffect, useState, useContext } from 'react';
 import PushNotifications from "./push-notifications/PushNotifications";
 
 function Notifications() {
-    const vapidPubKey = 'BFhZAN-nNturTa7ptaq5iR5hynPjKn5snJk6ZuSl0dcnURXiebicm6nhg7f-mnYdh5g4HfBbJuTusW6OLQ05mwY'
-
-    // const urlBase64ToUint8Array = (base64String) => {
-    //     const padding = "=".repeat((4 - base64String.length % 4) % 4);
-    //     const base64 = (base64String + padding)
-    //       .replace(/\-/g, "+")
-    //       .replace(/_/g, "/");
-
-    //     const rawData = window.atob(base64);
-    //     const outputArray = new Uint8Array(rawData.length);
-
-    //     for (let i = 0; i < rawData.length; ++i) {
-    //       outputArray[i] = rawData.charCodeAt(i);
-    //     }
-    //     return outputArray;
-    // }    
     return (<><PushNotifications /></>)
 }
 
